085c37be8cdb8b7f332406b703ad41ce9c455bde
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-18 19:15:17 +0300

    exercise 1 (level 1)

---
+++ Ex_1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+double x, y;
+Console.WriteLine("Введите значение X");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение Y");
+y = double.Parse(Console.ReadLine());
+
+double r = 2;
+
+if (Math.Abs(Math.Pow(x, 2) + Math.Pow(y, 2) - Math.Pow(r, 2)) <= Math.Pow(10, -3))
+{
+    Console.WriteLine($"Точка лежит на окружности с радиусом {r}");
+}
+else
+{
+    Console.WriteLine($"Точка не лежит на окружности с радиусом {r}");
+}
