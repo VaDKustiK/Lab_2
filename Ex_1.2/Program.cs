206767b60c1903ad4fea1380a881dc0b1bf80bbd
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-18 23:04:14 +0300

    exercise 2 (level 1)

---
+++ Ex_1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+double x, y;
+Console.WriteLine("Введите значение X");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение Y");
+y = double.Parse(Console.ReadLine());
+
+if (y >= 0 && y + Math.Abs(x) <= 1)
+{
+    Console.WriteLine("Точка лежит внутри фигуры");
+}
+else
+{
+    Console.WriteLine("Точка не лежит внутри фигуры");
+}
