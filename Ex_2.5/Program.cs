ea51f1b33da838d836f28585b5b26a5ca8bc21ca
Vadim Velikanov <vad.velikanov@yandex.ru>
2023-09-20 21:57:01 +0300

    exercise 5 (level 2)

---
+++ Ex_2.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+int n = 30, count = 0;
+double norm = 0;
+
+Console.WriteLine("Введите норматив (в минутах)");
+norm = double.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    double answer = double.Parse(Console.ReadLine());
+    if (answer >= norm)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"{count} спортсменов сдали норматив");
