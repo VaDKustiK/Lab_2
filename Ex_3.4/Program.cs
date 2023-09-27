d9fb166403203171fe40d4e876a484ac43faabf7
kust <vad.velikanov@yandex.ru>
2023-09-27 18:56:16 +0300

    exercise 4 (level 3)

---
+++ Ex_3.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+double n = 5;
+double r1 = 1;
+double r2 = 3;
+int count = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    double x, y;
+    Console.WriteLine($"Введите значение X для {i} точки");
+    x = double.Parse(Console.ReadLine());
+    Console.WriteLine($"Введите значение Y для {i} точки");
+    y = double.Parse(Console.ReadLine());
+
+    if (((x*x + y*y) >= r1 * r1) && ((x * x + y * y) <= r2 * r2))
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество точек: {count}");
+Console.ReadLine();
